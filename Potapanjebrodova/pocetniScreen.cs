7a51fed762d4fb508dfaf0d612e35fc708934845
Bojan Stetic <steticbojan@gmail.com>
2024-01-17 03:07:23 +0100

    zaboravio ovo

---
+++ Potapanjebrodova/pocetniScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Potapanjebrodova
+{
+    public partial class pocetniScreen : Form
+    {
+        public pocetniScreen()
+        {
+            InitializeComponent();
+            /*
+             * Odigrano igara
+             * Postotak Pobjede
+             * Pobjede
+             * Porazi
+             * Potopljeno protivnika
+             * Potonulo brodova
+             * Pogoci
+             * Postotak Pogodaka
+             */
+            string[] lines = File.ReadAllLines("userStats.txt");
+            string currUserStats = string.Join("\r\n", lines);
+            userStats.Text = currUserStats;
+        }
+    }
+}
