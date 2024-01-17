0dc289162673558ed48551dd1177628cd1713d7d
Bojan Stetic <steticbojan@gmail.com>
2024-01-17 03:06:45 +0100

    dio pocetnog screena

--- Potapanjebrodova/Properties/Resources.Designer.cs
+++ Potapanjebrodova/Properties/Resources.Designer.cs
@@ -179,5 +179,15 @@ namespace Potapanjebrodova.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pocetniBackground {
+            get {
+                object obj = ResourceManager.GetObject("pocetniBackground", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
